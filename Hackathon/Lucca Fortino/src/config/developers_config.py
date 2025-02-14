ea25aab70e4039ed477965437559f2a313e172ad
Lucca Fortino <luccafortino@gmail.com>
2025-02-13 21:11:07 -0300

    devs infos

--- Hackathon/Lucca Fortino/src/config/developers_config.py
+++ Hackathon/Lucca Fortino/src/config/developers_config.py
@@ -11,21 +11,21 @@ DEVELOPERS = [
     {
         "name": "Rafael Henrique de Souza",
         "ra": "356292",
-        "linkedin": "https://www.linkedin.com/in/"
+        "linkedin": ""
     },
      {
         "name": "Fabio Ferreira Scaion",
         "ra": "354943",
-        "linkedin": "https://www.linkedin.com/in/"
+        "linkedin": ""
     },
      {
         "name": "Lucas Picasso Botino",
-        "ra": "",
-        "linkedin": "https://www.linkedin.com/in/"
+        "ra": "355619",
+        "linkedin": "https://www.linkedin.com/in/lucas.picasso"
     },
      {
         "name": "Paulo Roberto Mota",
         "ra": "355014",
-        "linkedin": "https://www.linkedin.com/in/"
+        "linkedin": "https://www.linkedin.com/in/opaulomota"
     }
 ]
